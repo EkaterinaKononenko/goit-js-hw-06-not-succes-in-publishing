b9842818119a78fad83d61cb7abe02f510b3b614
EkaterinaKononenko <kaktysik4@ukr.net>
2022-09-12 16:17:05 +0300

    task 4 added

--- src/js/task-04.js
+++ src/js/task-04.js
@@ -9,3 +9,18 @@
 Создай переменную counterValue в которой будет храниться текущее значение счетчика и инициализируй её значением 0.
 Добавь слушатели кликов на кнопки, внутри которых увеличивай или уменьшай значение счтетчика.
 Обновляй интерфейс новым значением переменной counterValue.*/
+
+const object = {
+  incBtn: document.querySelector("[data-action='increment']"),
+  decBtn: document.querySelector("[data-action='decrement']"),
+};
+let counterValue = 0;
+const increment = () => {
+  document.getElementById("value").textContent = counterValue += 1;
+};
+
+const decrement = () => {
+  document.getElementById("value").textContent = counterValue -= 1;
+};
+object.incBtn.addEventListener("click", increment);
+object.decBtn.addEventListener("click", decrement);
